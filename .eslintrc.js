25d6b01b0a57b93cf8d2b7ec6e6da32a0df16b89
Yauheni <kirichenkozhenia@mail.ru>
2021-12-02 14:10:45 +0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'jsx-quotes': ['error', 'prefer-single'],
+    'no-param-reassign': ['error', { props: false }],
+    'react/jsx-props-no-spreading': ['off'],
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'jsx-a11y/no-static-element-interactions': ['off'],
+    'jsx-a11y/no-autofocus': ['off'],
+    'jsx-a11y/label-has-associated-control': ['off'],
+    'jsx-a11y/click-events-have-key-events': ['off'],
+    'jsx-a11y/no-noninteractive-element-interactions': ['off'],
+  },
+};
