8f97509947d2cf2787daa6d1c639ec926d4cd1e7
Yauheni <kirichenkozhenia@mail.ru>
2021-12-04 15:32:15 +0300

    feature: add react router to the project

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import MainFooter from '../main-footer/main-footer';
 import MainHeader from '../main-header/main-header';
+import Catalog from '../catalog/catalog';
+import Basket from '../basket/basket';
+import { page } from '../../constants';
 
 const App = () => (
   <div className='page'>
     <MainHeader />
+    <Routes>
+      <Route
+        exact
+        path={page.catalog.path}
+        element={
+          <Catalog />
+        }
+      />
+      <Route
+        exact
+        path={page.basket.path}
+        element={
+          <Basket />
+        }
+      />
+    </Routes>
     <MainFooter />
   </div>
 );
