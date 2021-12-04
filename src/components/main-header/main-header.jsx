7071f63a833120adf68789809ca944cd23428647
Yauheni <kirichenkozhenia@mail.ru>
2021-12-04 15:42:40 +0300

    feature: add routing to main header component

--- src/components/main-header/main-header.jsx
+++ src/components/main-header/main-header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LogoImage from '../../img/main-header/logo-black.svg';
 import LocationIcon from '../../img/main-header/icons/location-icon.svg';
 import SearchIcon from '../../img/main-header/icons/search-icon.svg';
 import BasketIcon from '../../img/main-header/icons/basket-icon.svg';
 import './main-header.scss';
+import { page } from '../../constants';
 
 const MainHeader = () => (
   <header className='main-header'>
@@ -12,7 +14,7 @@ const MainHeader = () => (
         <img className='main-header__logo-image' src={LogoImage} alt='Логотип Guitar shop' width='67' height='70' />
       </a>
       <ul className='main-header__main-menu'>
-        <li><a className='main-header__main-menu-link' href='/#'>Каталог</a></li>
+        <li><Link className='main-header__main-menu-link' to={page.catalog.path}>Каталог</Link></li>
         <li><a className='main-header__main-menu-link' href='/#'>Где купить?</a></li>
         <li><a className='main-header__main-menu-link' href='/#'>О компании</a></li>
         <li><a className='main-header__main-menu-link' href='/#'>Cервис-центры</a></li>
@@ -29,10 +31,10 @@ const MainHeader = () => (
           </a>
         </li>
         <li>
-          <a className='main-header__user-menu-link' href='/#'>
+          <Link className='main-header__user-menu-link' to={page.basket.path}>
             <img className='main-header__user-menu-icon' src={BasketIcon} alt='Корзина товаров' width='16' height='18' />
             <sup className='main-header__products-count'>2</sup>
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
